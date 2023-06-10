ac0a94cd23fd93d2b295df74d6e8b973ef298b6d
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-06-10 16:31:59 +0800

    Basic bot setup

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('egg'):
+        await message.channel.send('egg')
+        
+client.run(token)
