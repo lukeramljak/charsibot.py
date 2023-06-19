de0f51f681fd7fd5e61514ccf5da8e58b6fdc424
Luke Ramljak <lukeramljak@proton.me>
2023-06-19 21:20:27 +0800

    Clean up cogs loop

--- src/bot.py
+++ src/bot.py
@@ -14,9 +14,9 @@ async def on_ready():
     await client.change_presence(activity=activity)
     print(f'Logged in as {client.user.name}')
 
-for foldername in os.listdir('src/cogs'): # for every folder in cogs
-    for filename in os.listdir(f'src/cogs/{foldername}'): # for every file in a folder in cogs
-        if filename.endswith('.py') and not filename in ['util.py', 'error.py']: # if the file is a python file and if the file is a cog
+for foldername in os.listdir('./cogs'): # for every folder in cogs
+    for filename in os.listdir(f'./cogs/{foldername}'): # for every file in a folder in cogs
+        if filename.endswith('.py'): # if the file is a python file and if the file is a cog
             client.load_extension(f'cogs.{foldername}.{filename[:-3]}') # load the extension
         
 client.run(token)
