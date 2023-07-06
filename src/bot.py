c697e9a82fcf2ebdb66263d30fda30f5c7dcf835
Luke Ramljak <lukeramljak@proton.me>
2023-07-06 21:06:07 +0800

    Apply formatter

--- src/bot.py
+++ src/bot.py
@@ -3,20 +3,22 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-token = os.getenv('TOKEN')
-guild_id = os.getenv('GUILD_ID')
+token = os.getenv("TOKEN")
+guild_id = os.getenv("GUILD_ID")
 
 bot = discord.Bot(intents=discord.Intents.all())
-activity = discord.Activity(name='Big Chungus', type=discord.ActivityType.listening)
+activity = discord.Activity(name="Big Chungus", type=discord.ActivityType.listening)
+
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=activity)
-    print(f'Logged in as {bot.user}')
+    print(f"Logged in as {bot.user}")
 
-for folder in os.listdir('./cogs'): 
-    for file in os.listdir(f'./cogs/{folder}'):
-        if file.endswith('.py'):
-            bot.load_extension(f'cogs.{folder}.{file[:-3]}')
-        
+
+for folder in os.listdir("./cogs"):
+    for file in os.listdir(f"./cogs/{folder}"):
+        if file.endswith(".py"):
+            bot.load_extension(f"cogs.{folder}.{file[:-3]}")
+
 bot.run(token)
