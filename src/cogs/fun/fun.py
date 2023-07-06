2f2b10d5a42943e0549ee1557c0da7f2e575b9c1
Luke Ramljak <lukeramljak@proton.me>
2023-07-06 21:14:02 +0800

    Remove responses

--- src/cogs/fun/fun.py
+++ src/cogs/fun/fun.py
@@ -1,6 +1,7 @@
+from magic_8ball import responses
 import discord
-from discord.ext import commands
 from discord.commands import slash_command
+from discord.ext import commands
 import os
 import random
 import requests
@@ -14,39 +15,6 @@ class Fun(commands.Cog):
 
     @slash_command(description="Ask the magic 8-ball a question", guild_ids=[guild_id])
     async def magic8ball(self, ctx, question: str):
-        responses = [
-            "It is certain.",
-            "It is decidedly so.",
-            "Without a doubt.",
-            "Yes, definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful.",
-            "No way.",
-            "Maybe",
-            "The answer is hiding inside you",
-            "No.",
-            "||No||",
-            "||Yes||",
-            "Hang on",
-            "It's over",
-            "It's just the beginning",
-            "Good luck",
-        ]
-
         response = random.choice(responses)
         await ctx.respond(f"{question}\n\n 🎱 {response}")
 
