6225e28c937dcb71a58843c4286b2510ce187a9f
Luke Ramljak <lukeramljak@proton.me>
2023-07-09 18:00:32 +0800

    Add cooldown error handling

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
@@ -14,6 +15,16 @@ activity = discord.Activity(name="Big Chungus", type=discord.ActivityType.listen
 async def on_ready():
     await bot.change_presence(activity=activity)
     print(f"Logged in as {bot.user}")
+
+
+@bot.event
+async def on_application_command_error(
+    ctx: discord.ApplicationContext, error: discord.DiscordException
+):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.respond("This command is currently on cooldown!")
+    else:
+        raise error
 
 
 for cog in ("fun", "utilities"):
